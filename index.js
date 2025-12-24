0db036fc99afbbc3eec33851141bac6f9137e3c1
Arissa Fram <arissafram@gmail.com>
2025-12-24 13:01:32 -0500

    init rock paper scissors

--- index.js
+++ index.js
@@ -60,4 +60,36 @@ window.addEventListener("load", () => {
     ball.style.animationPlayState = "paused";
     ball.style.cursor = "default";
   };
+
+  // Rock paper scissors game
+  const result = document.querySelector("#result");
+
+  document.querySelector("#rock").onclick = () => play("rock");
+  document.querySelector("#paper").onclick = () => play("paper");
+  document.querySelector("#scissors").onclick = () => play("scissors");
+
+  function play(player) {
+    const randomNumber = Math.random();
+
+    let computer;
+    if (randomNumber < 0.33) {
+      computer = "rock";
+    } else if (randomNumber < 0.66) {
+      computer = "paper";
+    } else {
+      computer = "scissors";
+    }
+
+    if (player === computer) {
+      result.innerText = "Tie! You both chose " + player;
+    } else if (
+      (player === "rock" && computer === "scissors") ||
+      (player === "paper" && computer === "rock") ||
+      (player === "scissors" && computer === "paper")
+    ) {
+      result.innerText = "You win! " + player + " beats " + computer;
+    } else {
+      result.innerText = "You lose! " + computer + " beats " + player;
+    }
+  }
 });
