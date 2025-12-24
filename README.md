45cdbc331744962c88497e58e6cbecf3177cd6a7
Arissa Fram <arissafram@gmail.com>
2025-12-24 13:04:36 -0500

    init tic tac toe

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 ### List of games
 
-- rock paper scissors against the computer
-- farm animals
-- chaos buttons
 - race car game with two different key presses
 - bouncing ball game
-- matching game without shuffle
+- rock paper scissors against the computer
 - tic tac toe without game logic
+
+- farm animals
+- chaos buttons
+- matching game without shuffle
--- index.js
+++ index.js
@@ -61,6 +61,24 @@ window.addEventListener("load", () => {
     ball.style.cursor = "default";
   };
 
+  // Tic tac toe game
+  let currentPlayer = "X";
+  const turnText = document.querySelector("#turn");
+  const cells = document.querySelectorAll(".cell");
+
+  cells.forEach((cell) => {
+    cell.onclick = () => {
+      if (cell.innerText !== "") return;
+      cell.innerText = currentPlayer;
+      if (currentPlayer === "X") {
+        currentPlayer = "O";
+      } else {
+        currentPlayer = "X";
+      }
+      turnText.innerText = "Current player: " + currentPlayer;
+    };
+  });
+
   // Rock paper scissors game
   const result = document.querySelector("#result");
 
